ae29809fc9689164ac473c231871cc3e875b80aa
salcho-mirincom <salcho@mirincom.co.kr>
2022-07-28 15:47:28 +0900

    m

--- config/express.js
+++ config/express.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const compression = require("compression");
+//const compression = require("compression");
 const methodOverride = require("method-override");
 const fs = require('fs');
 const path = require('path');
 const HTTPS = require('https');
-var cors = require("cors");
+let cors = require("cors");
 
 module.exports = function () {
   const app = express();
@@ -16,8 +16,12 @@ module.exports = function () {
       cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/salcho.cf/cert.pem'), 'utf8').toString(),
     };
 
-    HTTPS.createServer(option, app).listen(sslport, () => {
-      console.success(`[HTTPS] Soda Server is started on port `);
+    HTTPS.createServer(option, app).listen( () => {
+      app.use(express.json()); // body값을 파싱
+      app.use(express.urlencoded({ extended: true })); // form 으로 제출되는 값 파싱
+      app.use(methodOverride()); // put, delete 요청 처리
+      app.use(cors()); // 웹브라우저 cors 설정을 관리
+      app.use(express.static("/var/www/html/salcho_front")); // express 정적 파일 제공 (html, css, js 등..)
     });
   } catch (error) {
     console.error('[HTTPS] HTTPS 오류가 발생하였습니다. HTTPS 서버는 실행되지 않습니다.');
@@ -25,12 +29,8 @@ module.exports = function () {
   }
 
   /* 미들웨어 설정 */
-  app.use(compression()); // HTTP 요청을 압축 및 해제
-  app.use(express.json()); // body값을 파싱
-  app.use(express.urlencoded({ extended: true })); // form 으로 제출되는 값 파싱
-  app.use(methodOverride()); // put, delete 요청 처리
-  app.use(cors()); // 웹브라우저 cors 설정을 관리
-  app.use(express.static("/var/www/html/salcho_front")); // express 정적 파일 제공 (html, css, js 등..)
+  //app.use(compression()); // HTTP 요청을 압축 및 해제
+
   // app.use(express.static(process.cwd() + '/public'));
 
   /* 직접 구현해야 하는 모듈 */
