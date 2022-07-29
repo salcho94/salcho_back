d3dd72c6c236343ebc1b313e237021ddc059b087
salcho-mirincom <salcho@mirincom.co.kr>
2022-07-29 16:47:09 +0900

    port change

--- index_server.js
+++ index_server.js
@@ -2,24 +2,23 @@ const express = require("./config/express");
 const { logger } = require("./config/winston"); // log
 const fs = require('fs');
 const path = require('path');
-const HTTP = require('http');
-const https = require('https');
-const PORT = process.env.PORT;
-
+const HTTPS = require('https');
 
 const app = express();
-const server = https.createServer(app);
+const port = 3000;
 try {
-    const options = {
+    const option = {
         ca: fs.readFileSync('/etc/letsencrypt/live/salcho.cf/fullchain.pem'),
         key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/salcho.cf/privkey.pem'), 'utf8').toString(),
         cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/salcho.cf/cert.pem'), 'utf8').toString(),
     };
 
-    https.createServer(options, app).listen(PORT);
-
-    } catch (error) {
-        console.error('[HTTPS] HTTPS 오류가 발생하였습니다. HTTPS 서버는 실행되지 않습니다.');
-        console.log(error);
+    HTTPS.createServer(option, app).listen(port, () => {
+        console.log(`[HTTPS] Soda Server is started on port ${console.log(port)}`);
+    });
+} catch (error) {
+    console.error('[HTTPS] HTTPS 오류가 발생하였습니다. HTTPS 서버는 실행되지 않습니다.');
+    console.log(error);
 }
 
+logger.info(`API Server Start At Port ${port}`);
