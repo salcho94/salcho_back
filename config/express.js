99913a3d90298b73800dd0aa39c6287ebdd096ef
salcho-mirincom <salcho@mirincom.co.kr>
2022-08-01 11:52:35 +0900

    test

--- config/express.js
+++ config/express.js
@@ -27,7 +27,7 @@ module.exports = function () {
   const httpServer = http.createServer(app);
   const httpsServer = https.createServer(credentials, app);
 
-  httpServer.listen(3000, () => {
+  httpServer.listen(80, () => {
     console.log('HTTP Server running on port 80');
   });
 
