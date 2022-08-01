0fee669f1f8c00c1ca2bd6be05615d49c330042f
salcho-mirincom <salcho@mirincom.co.kr>
2022-08-01 13:49:42 +0900

    test

--- index_server.js
+++ index_server.js
@@ -2,33 +2,23 @@ const express = require("./config/express");
 const { logger } = require("./config/winston"); // log
 const fs = require('fs');
 const path = require('path');
-const http = require('http');
-const https = require('https');
+const HTTPS = require('https');
 
 const app = express();
-// Certificate 인증서 경로
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/salcho.cf/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/salcho.cf/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/salcho.cf/chain.pem', 'utf8');
+const port = 3000;
+try {
+    const option = {
+        ca: fs.readFileSync('/etc/letsencrypt/live/salcho.cf/fullchain.pem'),
+        key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/salcho.cf/privkey.pem'), 'utf8').toString(),
+        cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/salcho.cf/cert.pem'), 'utf8').toString(),
+    };
 
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-};
+    HTTPS.createServer(option, app).listen(port, () => {
+        console.log(`[HTTPS] Soda Server is started on port ${console.log(port)}`);
+    });
+} catch (error) {
+    console.error('[HTTPS] HTTPS 오류가 발생하였습니다. HTTPS 서버는 실행되지 않습니다.');
+    console.log(error);
+}
 
-app.use((req, res) => {
-    res.send('Hello there !');
-});
-
-// Starting both http & https servers
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(3000, () => {
-    console.log('HTTP Server running on port 80');
-});
-
-httpsServer.listen(3001, () => {
-    console.log('HTTPS Server running on port 443');
-});
+logger.info(`API Server Start At Port ${port}`);
