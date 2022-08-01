acb3e8b1585e257c4bc38facd5f066d41dd2a84f
salcho-mirincom <salcho@mirincom.co.kr>
2022-08-01 11:01:22 +0900

    test

--- index_server.js
+++ index_server.js
@@ -8,9 +8,9 @@ const app = express();
 const port = 3000;
 try {
     const option = {
-        ca: fs.readFileSync('/etc/letsencrypt/live/salcho.cf/fullchain.pem'),
-        key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/salcho.cf/privkey.pem'), 'utf8').toString(),
-        cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/salcho.cf/cert.pem'), 'utf8').toString(),
+        ca: fs.readFileSync('/etc/letsencrypt/live/www.salcho.cf/fullchain.pem'),
+        key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/www.salcho.cf/privkey.pem'), 'utf8').toString(),
+        cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/www.salcho.cf/cert.pem'), 'utf8').toString(),
     };
 
     HTTPS.createServer(option, app).listen(port, () => {
