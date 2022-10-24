018c342d4655573bb0c2bed648d97b152b3f3b50
salcho-mirincom <salcho@mirincom.co.kr>
2022-10-24 20:15:09 +0900

    git ignore add

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,14 +1,30 @@
 const { pool } = require("../../config/database");
 const { logger } = require("../../config/winston");
 const requestIp = require('request-ip');
+const request = require('request');
 const secret = require("../../config/secret");
 const mainDao = require("../dao/mainDao");
+
+function doRequest(ip) {
+  return new Promise(function (resolve, reject) {
+    request(`http://ip-api.com/json/${ip}`, function (error, response) {
+      if (!error && response.statusCode == 200) {
+        resolve(response);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
 
 
 exports.visit = async function(req,res){
   try {
+    let request = require("request");
+
     console.log("client IP: " + requestIp.getClientIp(req));
     const ip = requestIp.getClientIp(req);
+
     const connection = await pool.getConnection(async (conn) => conn);
     if (!ip) {
       return res.send({
@@ -20,20 +36,26 @@ exports.visit = async function(req,res){
       try {
         const [row] = await mainDao.getVisit(connection, ip);
         let resIp = [];
+        let area = [];
         console.log(ip);
         if (row.length === 0) {
           resIp.push(ip);
           await mainDao.insertVisit(connection, ip)
         }
-        console.log('resIp',resIp);
+
+         let response = await doRequest(ip);
+         let str = JSON.parse(response.body);
+         area.push(str.country);
+        area.push(str.regionName);
+        area.push(str.city);
         const [today] = await mainDao.today(connection);
         const [total] = await mainDao.total(connection);
 
-        return res.send({
-          result: {today:today[0].todayCount,total:total[0].totalCount,ip: resIp[0]},
-          isSuccess: true,
-          code: 200,
-          message: "요청 성공",
+      return res.send({
+        result: {today:today[0].todayCount,total:total[0].totalCount,ip: resIp[0],area: area},
+        isSuccess: true,
+        code: 200,
+        message: "요청 성공",
         });
       } catch (err) {
         logger.error(`VISIT Query error\n: ${JSON.stringify(err)}`);
