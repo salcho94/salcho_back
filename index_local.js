7fe92b9d42247906dbf4adc7e7d5abba63253cc6
salcho-mirincom <salcho@mirincom.co.kr>
2022-10-28 10:52:20 +0900

    git ignore add

--- index_local.js
+++ index_local.js
@@ -1,7 +1,7 @@
 const express = require("./config/express");
 const { logger } = require("./config/winston"); // log
 
-const port = 3000;
+const port = 3005;
 express().listen(port,'0.0.0.0');
 logger.info(`API Server Start At Port ${port}`);
 
